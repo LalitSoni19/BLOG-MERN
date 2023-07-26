492c6ced159f50b4662b4aceec2ea1b5a7dcbd65
LalitSoni19 <lalit_soni_1008@outlook.com>
2023-07-26 18:43:03 +0530

    setting components

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,2 +1,11 @@
 import React from "react";
+import classes from './categories.module.css';
 
+const Categories = () => {
+    return (
+        <div>Categories</div>
+
+    )
+}
+
+export default Categories;
