ce3582a3b5170616313976111ad3b49515e68fe8
LalitSoni19 <lalit_soni_1008@outlook.com>
2023-07-26 18:57:44 +0530

    client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div >
-
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/create' element={<Create />} />
+        <Route path='/blogDetails' element={<BlogDetails />} />
+        <Route path='/updateBlog' element={<UpdateBlog />} />
+      </Routes>
     </div>
   );
 }
